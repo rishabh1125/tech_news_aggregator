5151e939526a0cc7343aa3233fb4ad68e2c58cd9
rishabh1125 <51257199+rishabh1125@users.noreply.github.com>
2021-08-12 09:46:46 +0530

    small changes

--- TechNewsAggregator/settings.py
+++ TechNewsAggregator/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w_%e7im$%4n=kxx6p^z=kqev+4rsru405)#sl22z%8#hy6a@71'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
