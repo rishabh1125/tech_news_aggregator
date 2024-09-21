adcda45c1fa2347e2f2fe9ab33ccac5081b2834c
rishabh pathak <prishabh83@gmail.com>
2024-09-22 01:35:28 +0530

    update logging

--- news/views.py
+++ news/views.py
@@ -2,30 +2,36 @@ import requests
 from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup as BSoup
 from news.models import Headline
+import logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG, 
+                       format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Create your views here.
 def scrape(request):
-    url = 'https://gadgets.ndtv.com/news'
-    p = requests.get(url)
-    pg  = p.content
-    soup = BSoup(pg, 'html.parser')
-    code = soup.findAll(class_ = 'thumb')
-    for i in range(20):
-        try:
-            img = code[i].find('img')['data-original']                
-        except:
-            img = code[i].find('img')['src']
-        title = code[i].find('img')['alt']
-        link = code[i].find('a')['href']
-        new_headline = Headline()
-        new_headline.title = title
-        new_headline.url = link
-        new_headline.image = img
-        try:
+    try:
+        logging.info()
+        url = 'https://gadgets.ndtv.com/news'
+        p = requests.get(url)
+        pg  = p.content
+        soup = BSoup(pg, 'html.parser')
+        code = soup.findAll(class_ = 'thumb')
+        for i in range(20):
+            img_data = code[i].find('img') 
+            if 'data-original' in img_data:    
+                img = img_data['data-original']                
+            else:
+                img = img_data['src']
+            title = code[i].find('img')['alt']
+            link = code[i].find('a')['href']
+            new_headline = Headline()
+            new_headline.title = title
+            new_headline.url = link
+            new_headline.image = img
             new_headline.save()
-        except:
-            pass
-    return redirect("../")    
+    except Exception as e:
+        logging.error(e)
+    finally:
+        return redirect("../")    
 
 def news_list(request):
     headlines = Headline.objects.all()[::-1]
